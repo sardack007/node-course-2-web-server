fe01079ae8895bacfe7afabb9be72938965f22d9
Jim Mac <jimmyenmanuel7@gmail.com>
2019-01-29 09:50:18 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`
+	console.log(log);
+	fs.appendFile('server.log',log + '\n',(err) => {
+		if(err){
+			console.log('Unable to appedn to server.log.');
+		}
+	});
+	next();
+});
+
+/* app.use((req,res, next) => {
+	res.render('maintence.hbs');
+}); */
+
+hbs.registerHelper('getCurrentYear',() => {
+	return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text) => {
+	return text.toUpperCase();
+});
+/*
+app.get('/', (req ,res) => {
+	
+	//res.send('<h1>Hello Express</h1>');
+	res.send({
+		name: 'Jim',
+		likes: [
+			'Biking',
+			'videogames'
+		]
+	});
+	
+	
+});
+*/
+app.get('/', (req, res) => {
+	res.render('home.hbs',{
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Home Page',
+		//currentYear: new Date().getFullYear()
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs',{
+		pageTitle: 'About Page',
+		//currentYear: new Date().getFullYear()
+	});
+});
+
+app.get('/bad', (req, res) => {
+	res.send({
+		message: 'The page is unable for the moment.'
+	});
+});
+
+app.listen(3000, () => {
+	console.log('server is up on port 3000');
+});
